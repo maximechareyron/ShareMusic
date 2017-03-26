e9f65cae5a19288d111240d2eff9c9f78febef15
olmartin2 <olivier.martin2@etu.udamail.fr>
2017-03-26 16:44:42 +0200

    ajout de la gestion de la playlist youtube, jouer une video au milieu de la playlist, suppression, cette derniere n est pas encore totalement fonctionnel, lorsque l on supprime une video cela fait recommencer la playlist depuis le debut, probleme affichage de la recylcerview : bouton superposé au titre

--- app/src/main/java/com/example/olmartin2/lecteurmusique/HebergeurActivity.java
+++ app/src/main/java/com/example/olmartin2/lecteurmusique/HebergeurActivity.java
@@ -4,6 +4,7 @@ package com.example.olmartin2.lecteurmusique;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
@@ -25,7 +26,14 @@ public class HebergeurActivity extends YouTubeBaseActivity implements YouTubePla
 
     //Button stop_play_button;
     private YouTubePlayerView youtubeView;
-    RecyclerView playlist;
+    static RecyclerView recyclerView ;
+    //Liste qui aura tous les ID des videos utilisé par player
+    static List<String> playlistID = new ArrayList<>();
+    //Liste qui sert a l'affichage pour la recyclerView afin de savoir quelle est le nom de la video
+    static List<String> playlistName = new ArrayList<>();
+
+    public Button refreshButton;
+    static YouTubePlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,35 +43,68 @@ public class HebergeurActivity extends YouTubeBaseActivity implements YouTubePla
         youtubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
         youtubeView.initialize(Config.YOUTUBE_API_KEY,this);
 
-        playlist = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView  = (RecyclerView) findViewById(R.id.list_music);
 
+        // Ici on instancie des videos pour le player Youtube, c'est ici qu'il faudra
+        // recevoir les vidéos avec firebase et les ajouter aux 2 listes
+        playlistName.add("Vivaldi");
+        playlistID.add("GRxofEmo3HA");
+        playlistName.add("Mozart");
+        playlistID.add("Rb0UmrCXxVA");
+        playlistName.add("Mozart requiem");
+        playlistID.add("Zi8vJ_lMxQI");
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(new PlaylistAdaptateur(playlistName,playlistID));
 
 
 
     }
 
 
-
-
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-        List<String> playlist = new ArrayList<>();
-        playlist.add("GRxofEmo3HA");
-        playlist.add("Rb0UmrCXxVA");
-        playlist.add("Zi8vJ_lMxQI");
+
+        this.player = youTubePlayer;
 
         YouTubeBaseActivity y = new YouTubeBaseActivity();
 
 
         if(!b){
-            youTubePlayer.cueVideos(playlist);
+            player.loadVideos(playlistID);
         }
     }
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        System.err.println("ça marche pas :(");
+    }
+
+    public static void changeMusic(String id){
+
+        for(int i=0; i<playlistID.size();i++){
+            if(playlistID.get(i).equals(id)){
+                player.cueVideos(playlistID,i,0);
+                player.play();
+            }
+        }
 
     }
 
+    public static void deleteMusic(String id){
+        int timeSafe = player.getCurrentTimeMillis();
+        String videoSafe;
 
+        for(int i=0; i<playlistID.size();i++){
+            if(playlistID.get(i).equals(id)){
+                playlistID.remove(i);
+                playlistName.remove(i);
+                recyclerView.setAdapter(new PlaylistAdaptateur(playlistName,playlistID));
+                player.loadVideos(playlistID);
+                return;
+            }
+        }
+        System.err.println("vidéo a supprimer inconnue");
+    }
 }
---
+++ app/src/main/java/com/example/olmartin2/lecteurmusique/PlaylistAdaptateur.java
@@ -0,0 +1,84 @@
+package com.example.olmartin2.lecteurmusique;
+
+/**
+ * Created by Olivier on 25/03/2017.
+ */
+import android.provider.ContactsContract;
+import android.support.v4.util.ArrayMap;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PlaylistAdaptateur extends RecyclerView.Adapter<PlaylistAdaptateur.ViewHolder>{
+    private List<String> playlistName;
+    private List<String> playlistID;
+
+
+    public PlaylistAdaptateur(List<String> playlistName, List<String> playlistID){
+
+        this.playlistName=playlistName;
+        this.playlistID=playlistID;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_playlist,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String itemName = playlistName.get(position);
+        String itemID = playlistID.get(position);
+        holder.bind(itemName,itemID);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return playlistName.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView monItemName;
+        public Button playButton;
+        public Button deleteButton;
+        public String monItemID;
+
+        public ViewHolder(View ItemView){
+            super(ItemView);
+
+            monItemName = (TextView) itemView.findViewById(R.id.item_playlist);
+            playButton = (Button) itemView.findViewById(R.id.item_play);
+            deleteButton = (Button) itemView.findViewById(R.id.item_delete);
+
+            playButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    //faire jouer la musique
+                    System.out.println("Nom video : " + monItemName.getText().toString() + " ID : " + monItemID);
+                    HebergeurActivity.changeMusic(monItemID);
+                }
+            });
+
+            deleteButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    HebergeurActivity.deleteMusic(monItemID);
+
+                }
+            });
+        }
+
+        public void bind(String itemName, String itemID){
+            monItemName.setText(itemName);
+            monItemID = itemID;
+
+        }
+    }
+}
